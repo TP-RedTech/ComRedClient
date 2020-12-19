ce87984f1fa4fb0945aca79c25d4398e427fd98f
Timonin Anton <dotruger.37@gmail.com>
2020-12-19 13:21:54 +0500

    Pull master

--- HTTPClient/include/client.h
+++ HTTPClient/include/client.h
@@ -7,49 +7,49 @@
 
 namespace client {
 enum class ClientErrors {
-  success,
-  failure
+    success,
+    failure
 };
 
 using ClientOut = std::pair<ClientErrors, std::string>;
 
 class Client {
 public:
-  ClientOut connect(int editorId, int docId);
-  ClientOut update(int editorId, int docId, int cursorPos, std::string operations);
-  ClientOut deleteDoc(int editorId, int docId);
+    ClientOut connect(int editorId, int docId);
+    ClientOut update(int editorId, int docId, int cursorPos, std::string operations);
+    ClientOut deleteDoc(int editorId, int docId);
 };
 }
 
 class Document {
 public:
-  void applyChanges(std::string operations);
-  const std::string &text() {
-    return text;
-  }
+    void applyChanges(std::string operations);
+    const std::string &text() {
+        return text;
+    }
 private:
-  std::string text;
+    std::string text;
 };
 
 class Application {
 public:
-  Application(int editorId, int docId) : myId(editorId), docId(docId) {}
-
-  void connect() {
-    client.connect(1, 1);
-    std::cout << "Connected" << std::endl;
-  }
-
-  void update(int cPos, std::string operations) {
-    auto res = client->update(myId, docId, cPos, operations);
-    doc->applyChanges(res.second);
-    std::cout << doc->text() << endl;
-  }
-
+    Application(int editorId, int docId) : myId(editorId), docId(docId) {}
+    
+    void connect() {
+        client.connect(1, 1);
+        std::cout << "Connected" << std::endl;
+    }
+    
+    void update(int cPos, std::string operations) {
+        auto res = client->update(myId, docId, cPos, operations);
+        doc->applyChanges(res.second);
+        std::cout << doc->text() << endl;
+    }
+    
 private:
-  std::shared_ptr<Document> doc;
-  int docId;
-  int myId;
-  std::shared_ptr<Client> client;
+    std::shared_ptr<Document> doc;
+    int docId;
+    int myId;
+    std::shared_ptr<Client> client;
 };
 #endif
